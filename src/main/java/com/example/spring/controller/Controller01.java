ffd43963d312748574420fee11f606483092e1ac
kkssii0210 <kkssii0210@naver.com>
2023-09-20 16:20:05 +0900

    first commit

---
+++ src/main/java/com/example/spring/controller/Controller01.java
@@ -0,0 +1,21 @@
+package com.example.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Controller01 {
+    @RequestMapping("/")
+    public void method1 () {
+        System.out.println("Controller01.method1");
+    }
+    @RequestMapping("/path1")
+    public void method2() {
+        System.out.println("Controller01.method2");
+    }
+    @RequestMapping("/path2")
+    public void method3() {
+        System.out.println("Controller01.method3");
+    }
+}
